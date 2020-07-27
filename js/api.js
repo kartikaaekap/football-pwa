41d221acb4c5c3eca0b214934cc78261f3330e6a
kartikaaekap <kartikaaekap@gmail.com>
2020-07-27 07:14:15 +0700

    Initial Commit

--- js/api.js
+++ js/api.js
@@ -37,6 +37,53 @@ function error(error) {
 
 // Blok kode untuk melakukan request data json
 function getStandings() {
+  if ("caches" in window) {
+    caches.match(standing_url).then(function(response) {
+      if (response) {
+        response.json().then(function(data) {
+            var standingsHTML =  `
+                <table style="font-size:14px;" class="responsive-table">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Logo</th>
+                      <th>CLUB</th>
+                      <th>PG</th>
+                      <th>W</th>
+                      <th>D</th>
+                      <th>L</th>
+                      <th>GF</th>
+                      <th>GA</th>
+                      <th>GD</th>
+                      <th>Pts</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+            `;
+            data.standings["0"].table.forEach(function(item) {
+              standingsHTML += `
+                      <tr>
+                        <td>${item.position}</td>
+                        <td><a href="./team.html?id=${item.team.id}"><img style="width:20px;" src="${item.team.crestUrl}"></a></td>
+                        <td><a href="./team.html?id=${item.team.id}">${item.team.name}</a></td>
+                        <td>${item.playedGames}</td>
+                        <td>${item.won}</td>
+                        <td>${item.draw}</td>
+                        <td>${item.lost}</td>
+                        <td>${item.goalsFor}</td>
+                        <td>${item.goalsAgainst}</td>
+                        <td>${item.goalDifference}</td>
+                        <td>${item.points}</td>
+                      </tr>
+              `;
+          });
+          standingsHTML += `</tbody>
+                  </table>`;
+          document.getElementById("standings").innerHTML = standingsHTML;
+        });
+      }
+    });
+  }
   fetchApi(standing_url)
     .then(status)
     .then(json)
@@ -90,9 +137,64 @@ function getTeamById() {
 
   var urlParams = new URLSearchParams(window.location.search);
   var idParam = urlParams.get("id");
-
   var team_id_url = `${base_url}teams/${idParam}`;
-
+  if ("caches" in window) {
+    caches.match(team_id_url).then(function(response) {
+      if (response) {
+        response.json().then(function(data) {
+          var teamHTML = `
+          <div class="row">
+            <h4 class="light center grey-text text-darken-3" style="font-size:40px; font-weight:bold;"><img style="width:90px;" src="${data.crestUrl}"> <br>${data.name}</h4>
+                  <p align="center">Nickname : ${data.shortName}<br>Address : ${data.address}<br>Founded : ${data.founded}<br>Club Colors : ${data.clubColors}<br>Venue : ${data.venue}</p>
+              <div class="col s12 m6 l6">
+                <div class="card-panel center" style="background-color: #0D47A1; color: white;">
+                <h5 style="font-weight:bold;">COMPETITIONS</h5>
+                  <p>
+                      <ul>
+            `;
+            data.activeCompetitions.forEach(function(item) {
+            teamHTML += `
+                      <li>${item.name}</li>
+                        `;
+            });
+            teamHTML += `
+                      </ul>
+                    </p>
+                  </div>
+                </div>
+                <div class="col s12 m6 l6">
+                <div class="card-panel center">
+                  <h5 style="font-size:25px; color:blue; font-weight:bold;">SQUAD</h5>
+                  <table style="font-size:14px;" class="responsive-table">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                        `;
+            data.squad.forEach(function(item) {
+            teamHTML += `
+                      <tr>
+                      <td>${item.name}</td>
+                      <td>${item.position}</td>
+                      </tr>
+                        `;
+            });
+            teamHTML += `
+    
+                </div>
+              </div>
+            </div>
+                        `;
+          document.getElementById("body-content").innerHTML = teamHTML;
+          // Kirim objek data hasil parsing json agar bisa disimpan ke indexed db
+          resolve(data);
+        });
+      }
+    });
+  }
   fetchApi(team_id_url)
     .then(status)
     .then(json)
@@ -183,6 +285,8 @@ function getSavedTeams() {
                button.addEventListener("click", function (event) {
                    let id = Number(button.value);
                    console.log(id);
+                   var toastHTML = '<span>Successfully remove the team from favorite</span><button onclick="M.Toast.getInstance(this.parentElement).dismiss()" class="btn-flat toast-action">Close</button>';
+                   M.toast({html: toastHTML});
                    dbDeleteTeam(id).then(() => {
                        getSavedTeams()
                    })
--- service-worker.js
+++ service-worker.js
@@ -1,73 +1,62 @@
-const CACHE_NAME = "football-pwa-v1";
-var urlsToCache = [
-  "/",
-  "/nav.html",
-  "/index.html",
-  "/team.html",
-  "/pages/home.html",
-  "/pages/ranking.html",
-  "/pages/favorite.html",
-  "/css/materialize.min.css",
-  "/css/footer.css",
-  "/css/home.css",
-  "/css/ranking.css",
-  "/js/materialize.min.js",
-  "/js/api.js",
-  "/js/nav.js",
-  "/js/db.js",
-  "/js/idb.js",
-  "/manifest.json",
-  "/images/background.jpg",
-  "/images/notif.png",
-  "/icon.png",
-  "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v52/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
  
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
+if (workbox){
+  console.log(`Workbox berhasil dimuat`);
+  workbox.precaching.precacheAndRoute([
+    { url: '/index.html', revision: '1' },
+    { url: '/nav.html', revision: '1' },
+    { url: '/team.html', revision: '1' },
+    { url: '/home.html', revision: '1' },
+    { url: '/favorite.html', revision: '1' },
+    { url: '/ranking.html', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/css/footer.css', revision: '1' },
+    { url: '/css/home.css', revision: '1' },
+    { url: '/css/ranking.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/js/api.js', revision: '1' },
+    { url: '/js/nav.js', revision: '1' },
+    { url: '/js/db.js', revision: '1'},
+    { url: '/js/idb.js', revision: '1' },
+    { url: '/manifest.json', revision: '1' },
+    { url: 'https://fonts.googleapis.com/icon?family=Material+Icons', revision: '1' },
+]);
+  workbox.routing.registerRoute(
+    /.*(?:png|gif|jpg|jpeg|svg|webp)$/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        }),
+        new workbox.expiration.Plugin({
+          maxEntries: 100,
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+      ]
     })
   );
-});
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function (cached) {
-      if (cached) {
-        return cached
-      }else {
-            caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-                return response || fetch (event.request);
-            }
-        )
-    }
-
-      return fetch(event.request).then(function(response) {
-        const responseToCahce = response.clone()
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request.url, responseToCahce);
-        })
-        return response;
+    workbox.routing.registerRoute(    
+    new RegExp('/'),
+    workbox.strategies.staleWhileRevalidate()
+  );
+    workbox.routing.registerRoute(
+      new RegExp('/pages/'),
+      workbox.strategies.staleWhileRevalidate()
+    );
+      workbox.routing.registerRoute(
+      new RegExp('https://api.football-data.org/v2/'),
+      workbox.strategies.staleWhileRevalidate({
+        cacheExpiration: {
+              maxAgeSeconds: 60 * 30 //cache diperbarui setiap 30 menit
+        }
       })
-    })
- );
-});
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
-});
+}
+
+else{
+  console.log(`Workbox gagal dimuat`);
+}
 
 self.addEventListener('push', function(event) {
   var body;
